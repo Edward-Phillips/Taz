dfff101dab2a7c75899a533703dd2ee46b0eb139
edwardPhillips <edward@twine.fm>
2022-10-04 20:08:05 +0100

    all tests passing, fizzbuzz!!!

--- __tests__/fizzbuzz.test.js
+++ __tests__/fizzbuzz.test.js
@@ -13,3 +13,37 @@ test('fizzbuzz returns 2 when given 2', () => {
   const input = 2;
   expect(fizzbuzz(input)).toBe(2);
 })
+
+test('fizzbuzz returns fizz when given 3', () => {
+  const input = 3;
+  expect(fizzbuzz(input)).toBe("fizz")
+})
+test('fizzbuzz returns fizz when given 4', () => {
+  const input = 4;
+  expect(fizzbuzz(input)).toBe(4);
+})
+
+test('fizzbuzz returns buzz when given 5', () => {
+  const input = 5;
+  expect(fizzbuzz(input)).toBe("buzz")
+})
+
+test('fizzbuzz returns fizzbuzz when given 15', () => {
+  const input = 15;
+  expect(fizzbuzz(input)).toBe("fizzbuzz");
+})
+
+test('fizzbuzz returns fizz when given 6', () => {
+  const input = 6;
+  expect(fizzbuzz(input)).toBe("fizz")
+})
+
+test('fizzbuzz returns buzz when given 5', () => {
+  const input = 10;
+  expect(fizzbuzz(input)).toBe("buzz")
+})
+
+test('fizzbuzz returns fizzbuzz when given 15', () => {
+  const input = 45;
+  expect(fizzbuzz(input)).toBe("fizzbuzz");
+})
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,16 @@
 function fizzbuzz(input){
-  return input};
+  if (input % 15 === 0) {
+    return "fizzbuzz"
+  }
+  if (input % 3 === 0) {
+    return "fizz";
+  } else if (input === 5) {
+    return "buzz"
+  }
+  if (input % 5 === 0) {
+    return "buzz"
+  }
+  return input}
 
 
 module.exports = fizzbuzz;
